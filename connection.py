02c26be52e9b53efdb2b0dbed938439cc06ae9c6
Beatryz Couto <61602714+coutobeatryz@users.noreply.github.com>
2025-08-18 09:10:54 -0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -2,10 +2,11 @@ import psycopg2
 
 def get_connection():
     conn = psycopg2.connect(
-        dbname="MinhaFichaTeste",
+        dbname="MinhaFicha",
         user="postgres",
         password="password",
         host="localhost",
         port="5432"
     )
+
     return conn
