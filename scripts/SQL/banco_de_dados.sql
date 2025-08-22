b3fd67304e4f184000bc271ed9e89849fa3383c4
Beatryz Couto <61602714+coutobeatryz@users.noreply.github.com>
2025-08-22 13:40:32 -0300

    Update banco_de_dados.sql

--- scripts/SQL/banco_de_dados.sql
+++ scripts/SQL/banco_de_dados.sql
@@ -29,15 +29,6 @@ CREATE TABLE academico.professores (
     CONSTRAINT un_professores_registro UNIQUE (numero_registro)
 );
 
-CREATE TABLE academico.grades_horarias (
-    grade_id SERIAL NOT NULL, 
-    aluno_id INTEGER NOT NULL,
-    horarios VARCHAR(255),
-    CONSTRAINT pk_grades_horarias PRIMARY KEY (grade_id),
-    CONSTRAINT fk_grades_alunos FOREIGN KEY (aluno_id) REFERENCES academico.alunos(aluno_id) ON DELETE CASCADE,
-    CONSTRAINT un_grades_aluno UNIQUE (aluno_id)
-);
-
 CREATE TABLE academico.disciplinas (
     disciplina_id SERIAL NOT NULL,
     nome VARCHAR(255) NOT NULL,
@@ -50,21 +41,47 @@ CREATE TABLE academico.disciplinas (
     CONSTRAINT un_disciplinas_codigo UNIQUE (codigo)
 );
 
-CREATE TABLE academico.registros_presenca (
-    disciplina_id INTEGER NOT NULL,
-    data_presenca DATE NOT NULL,
-    CONSTRAINT pk_registros_presenca PRIMARY KEY (disciplina_id, data_presenca)
-);
-
 CREATE TABLE academico.atividades (
     atividade_id SERIAL NOT NULL,
     titulo VARCHAR(255) NOT NULL,
     descricao VARCHAR(500),
-    status VARCHAR(50),
+    status VARCHAR(50) DEFAULT 'A FAZER',
     data DATE,
-    disciplina_id INTEGER,
-    professor_id INTEGER,
+
+    tipo VARCHAR(20) NOT NULL DEFAULT 'PESSOAL', 
+    disciplina_id INTEGER NULL,                  
+    professor_id INTEGER NULL,                    
+    aluno_criador_id INTEGER NULL,               
+
     CONSTRAINT pk_atividades PRIMARY KEY (atividade_id)
+);
+
+CREATE TABLE academico.alunos_atividades (
+    aluno_id INTEGER NOT NULL,
+    atividade_id INTEGER NOT NULL,
+    CONSTRAINT pk_alunos_atividades PRIMARY KEY (aluno_id, atividade_id)
+);
+
+
+CREATE TABLE academico.grades_horarias (
+    grade_id SERIAL NOT NULL,
+    aluno_id INTEGER NOT NULL,
+    horarios VARCHAR(255),
+    CONSTRAINT pk_grades_horarias PRIMARY KEY (grade_id),
+    CONSTRAINT fk_grades_alunos FOREIGN KEY (aluno_id) REFERENCES academico.alunos(aluno_id) ON DELETE CASCADE,
+    CONSTRAINT un_grades_aluno UNIQUE (aluno_id)
+);
+
+CREATE TABLE academico.grades_disciplinas (
+    grade_id INTEGER NOT NULL,
+    disciplina_id INTEGER NOT NULL,
+    CONSTRAINT pk_grades_disciplinas PRIMARY KEY (grade_id, disciplina_id)
+);
+
+CREATE TABLE academico.registros_presenca (
+    disciplina_id INTEGER NOT NULL,
+    data_presenca DATE NOT NULL,
+    CONSTRAINT pk_registros_presenca PRIMARY KEY (disciplina_id, data_presenca)
 );
 
 CREATE TABLE academico.atividades_avaliativas (
@@ -82,23 +99,18 @@ CREATE TABLE academico.tarefas (
     CONSTRAINT fk_tarefas_atividades FOREIGN KEY (atividade_id) REFERENCES academico.atividades(atividade_id) ON DELETE CASCADE
 );
 
-CREATE TABLE academico.alunos_atividades (
-    aluno_id INTEGER NOT NULL,
-    atividade_id INTEGER NOT NULL,
-    CONSTRAINT pk_alunos_atividades PRIMARY KEY (aluno_id, atividade_id)
-);
-
-CREATE TABLE academico.grades_disciplinas (
-    grade_id INTEGER NOT NULL,
-    disciplina_id INTEGER NOT NULL,
-    CONSTRAINT pk_grades_disciplinas PRIMARY KEY (grade_id, disciplina_id)
-);
 
 ALTER TABLE academico.disciplinas ADD CONSTRAINT fk_disciplinas_professores
 FOREIGN KEY (professor_id) REFERENCES academico.professores(professor_id) ON DELETE SET NULL;
 
 ALTER TABLE academico.disciplinas ADD CONSTRAINT fk_disciplinas_prerequisito
 FOREIGN KEY (pre_requisito_id) REFERENCES academico.disciplinas(disciplina_id) ON DELETE SET NULL;
+
+ALTER TABLE academico.grades_disciplinas ADD CONSTRAINT fk_gd_grade
+FOREIGN KEY (grade_id) REFERENCES academico.grades_horarias(grade_id) ON DELETE CASCADE;
+
+ALTER TABLE academico.grades_disciplinas ADD CONSTRAINT fk_gd_disciplina
+FOREIGN KEY (disciplina_id) REFERENCES academico.disciplinas(disciplina_id) ON DELETE CASCADE;
 
 ALTER TABLE academico.registros_presenca ADD CONSTRAINT fk_presenca_disciplina
 FOREIGN KEY (disciplina_id) REFERENCES academico.disciplinas(disciplina_id) ON DELETE CASCADE;
@@ -109,13 +121,12 @@ FOREIGN KEY (disciplina_id) REFERENCES academico.disciplinas(disciplina_id) ON D
 ALTER TABLE academico.atividades ADD CONSTRAINT fk_atividades_professores
 FOREIGN KEY (professor_id) REFERENCES academico.professores(professor_id) ON DELETE SET NULL;
 
+ALTER TABLE academico.atividades ADD CONSTRAINT fk_atividades_aluno_criador
+FOREIGN KEY (aluno_criador_id) REFERENCES academico.alunos(aluno_id) ON DELETE SET NULL;
+
+
 ALTER TABLE academico.alunos_atividades ADD CONSTRAINT fk_aa_aluno
 FOREIGN KEY (aluno_id) REFERENCES academico.alunos(aluno_id) ON DELETE CASCADE;
+
 ALTER TABLE academico.alunos_atividades ADD CONSTRAINT fk_aa_atividade
 FOREIGN KEY (atividade_id) REFERENCES academico.atividades(atividade_id) ON DELETE CASCADE;
-
-ALTER TABLE academico.grades_disciplinas ADD CONSTRAINT fk_gd_grade
-FOREIGN KEY (grade_id) REFERENCES academico.grades_horarias(grade_id) ON DELETE CASCADE;
-
-ALTER TABLE academico.grades_disciplinas ADD CONSTRAINT fk_gd_disciplina
-FOREIGN KEY (disciplina_id) REFERENCES academico.disciplinas(disciplina_id) ON DELETE CASCADE;
